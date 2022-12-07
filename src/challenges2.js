fcfcd2e42ae9d91cbb3ec50d334dea563c659fd1
jefersonviana <jefersonv28@gmail.com>
2022-12-07 10:50:59 -0300

    Requisito 11 concluído

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,5 +1,26 @@
 // Desafio 11 - Crie a função generatePhoneNumber
+function generatePhoneNumber(number) {
+  if (number.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index = 0; index < number.length; index += 1) {
+    let recebe = 0;
+    if (number[index] < 0 || number[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    for (let index1 = 0; index1 < number.length; index1 += 1) {
+      if (number[index] === number[index1]) {
+        recebe += 1;
+        if (recebe === 3) {
+          return 'não é possível gerar um número de telefone com esses valores';
+        }
+      }
+    }
+  }
+  return `(${number[0]}${number[1]}) ${number[2]}${number[3]}${number[4]}${number[5]}${number[6]}-${number[7]}${number[8]}${number[9]}${number[10]}`;
+}
 
+console.log(generatePhoneNumber([7, 5, 4, 8, 9, 6, 1, 2, 3, 6, 8]));
 // Desafio 12 -  Crie a função triangleCheck
 
 // Desafio 13 - Crie a função hydrate
